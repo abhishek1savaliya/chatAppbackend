19ef267df90e786c9d6d50b19128d76c59252aab
abhisheksavaliya555 <abhisheksavaliya555@gmail.com>
2024-08-05 14:16:48 +0530

    add log

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes/route');
+const morgan = require('morgan')
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const PORT = process.env.PORT || 5000;
@@ -8,6 +9,8 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 const app = express();
+
+app.use(morgan('tiny'))
 
 app.use(bodyParser.json());
 connectDB();
